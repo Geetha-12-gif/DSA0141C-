183f4abd12f94815245511022b47f467e0d2c7f6
Geetha-12-gif <113919199+Geetha-12-gif@users.noreply.github.com>
2023-02-01 21:37:26 +0530

    Create sum of two numbers.cpp
    enter x and y value3
    4
    sum of two numbers7
    --------------------------------
    Process exited after 3.754 seconds with return value 0
    Press any key to continue . . .

---
+++ sum of two numbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class sum
+{
+	int x,y;
+	public:
+		void getdata()
+		{
+			cout<<"enter x and y value";
+			cin>>x>>y;
+		}
+		void display()
+		{
+			cout<<"sum of two numbers"<<x+y;
+		}
+};
+int main()
+{
+	sum s;
+	s.getdata();
+	s.display();
+}
