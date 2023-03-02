39e94e3a623ad219eb068e59c2a52c28457cacdd
Geetha-12-gif <113919199+Geetha-12-gif@users.noreply.github.com>
2023-03-02 21:10:35 +0530

    Add files via upload
    enter the number5
    is a prime number
    --------------------------------
    Process exited after 2.045 seconds with return value 0
    Press any key to continue . . .

---
+++ prime or not.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i;
+	cout<<"enter the number";
+	cin>>n;
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<"is not a prime number";
+		}
+		else
+		{
+			cout<<"is a prime number";
+		}
+	}
+}
